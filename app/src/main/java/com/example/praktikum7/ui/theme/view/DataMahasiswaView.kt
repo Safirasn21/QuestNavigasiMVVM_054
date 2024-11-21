049649ba9c57f0b549fd9f8180c2e3edec782f8d
safirasn21 <safirasn021@gmai.com>
2024-11-21 18:57:55 +0700

    memperbaiki tampilan DataMahasiswaView dengan menambahkan padding dan kolom

--- app/src/main/java/com/example/praktikum7/ui/theme/view/DataMahasiswaView.kt
+++ app/src/main/java/com/example/praktikum7/ui/theme/view/DataMahasiswaView.kt
@@ -1,8 +1,14 @@
 package com.example.praktikum7.ui.theme.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.praktikum7.model.DataMahasiswa
 
 @Composable
@@ -23,4 +29,18 @@ fun DataMahasiswaView(
             isiParam = items.second
         ) }
     }
-}
+
+    @Composable
+    fun CardSection(judulParam: String, isiParam:String){
+        Column(){
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = judulParam, modifier = Modifier.weight(0.8f))
+                Text(text = " = ", modifier = Modifier.weight(0.2f))
+                Text(text = "$isiParam",
+                    modifier = Modifier.weight(2f))
+            }
+        }
+    }
