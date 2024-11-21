74a2ec08d970f0fc9457723801f3409cd7392032
safirasn21 <safirasn021@gmai.com>
2024-11-21 19:01:27 +0700

    Menambahkan fungsi FormMahasiswaView untuk form input data mahasiswa

---
+++ app/src/main/java/com/example/praktikum7/ui/theme/view/FormMahasiswaView.kt
@@ -0,0 +1,62 @@
+package com.example.praktikum7.ui.theme.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormMahasiswaView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
+){
+    var nama by rememberSaveable { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var noHP by remember { mutableStateOf("") }
+    var selectedGender by remember { mutableStateOf("") }
+    val jenisKelamin = listOf("Laki-laki", "Perempuan")
+
+    val dataMahasiswa: MutableList<String> = mutableListOf(nama, selectedGender, alamat)
+
+    var namaUser by rememberSaveable { mutableStateOf("") }
+    var emailUser by remember { mutableStateOf("") }
+    var alamatUser by remember { mutableStateOf("") }
+    var noHPUser by remember { mutableStateOf("") }
+    var selectedGenderUser by remember { mutableStateOf("") }
+
+    Column (
+        Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        OutlinedTextField(value = nama,
+            onValueChange = {nama = it},
+            placeholder = { Text("Masukkan namamu") },
+            label = { Text("Nama") },
+            modifier= Modifier.fillMaxWidth().padding(5.dp))
+
+        Row (){
+            listJK.forEach { item ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(selected = selectedGender == item,
+                        onClick = {
+                            selectedGender = item
+                        })
+                    Text(item)
+                }
+            }
+        }
